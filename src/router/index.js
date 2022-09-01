d940a2b854d4a827267a63c38da5cc1e43b841b6
[Kene Nwogu] <nwogujoseph1111@gmail.com>
2022-09-01 07:36:34 +0100

    added register view to routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import TweetView from "@/views/TweetView.vue";
+import RegisterView from "@/views/RegisterView.vue";
 
 const routes = [
     {
@@ -12,6 +13,11 @@ const routes = [
         path: '/tweet/:id',
         name: 'Tweet',
         component: TweetView
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: RegisterView
     }
 ]
 
